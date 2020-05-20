456ada4001e9f4f7eba1f4aaeb3db8445dd01d65
Jake Stambaugh <jake.stambaugh@gmail.com>
2020-05-20 03:23:23 +0000

    Add initializers

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -79,7 +79,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Map<String, LoxFunction> methods = new HashMap<>();
     for (Stmt.Function method : stmt.methods) {
       LoxFunction function = new LoxFunction(method, environment);
-      methods.put(method.name.lexeme, function);
+      methods.put(method.name.lexeme, function, method.name.lexeme.equals("init"));
     }
 
     LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
@@ -95,7 +95,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
-    LoxFunction function = new LoxFunction(stmt, environment);
+    LoxFunction function = new LoxFunction(stmt, environment, false);
     environment.define(stmt.name.lexeme, function);
     return null;
   }
--- java/com/craftinginterpreters/lox/Resolver.java
+++ java/com/craftinginterpreters/lox/Resolver.java
@@ -17,8 +17,16 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    INITIALIZER,
     METHOD
   }
+
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private ClassType currentClass = ClassType.NONE;
 
   @Override
   public Void visitBlockStmt(Stmt.Block stmt) {
@@ -30,6 +38,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+
     declare(stmt.name);
     define(stmt.name);
 
@@ -38,11 +48,15 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
       resolveFunction(method, declaration);
     }
 
     endScope();
 
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -73,6 +87,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword,
+            "Cannot return a value from an initializer.");
+      }
       resolve(stmt.value);
     }
 
@@ -163,6 +181,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword, 
+          "Cannot use 'this' outside of a class.");
+      return null;
+    }
+
     resolveLocal(expr, expr.keyword);
     return null;
   }
