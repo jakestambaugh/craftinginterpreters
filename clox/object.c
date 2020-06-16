59ef5303e4bfbd944be7c9b5a0f8a16bdf0a8c65
Jake Stambaugh <jake.stambaugh@gmail.com>
2020-06-16 19:35:38 -0400

    There's a bug that lives in local_up

--- clox/object.c
+++ clox/object.c
@@ -20,7 +20,7 @@ static Obj* allocateObject(size_t size, ObjType type) {
 }
 
 ObjClosure* newClosure(ObjFunction* function) {
-  ObjClosure** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
+  ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
   for (int i = 0; i < function->upvalueCount; i++) {
     upvalues[i] = NULL;
   }
