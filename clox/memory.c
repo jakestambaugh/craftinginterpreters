41a67ddd6dd4a207c68bbb2485fbfaa8a3244fab
Jake Stambaugh <jake.stambaugh@gmail.com>
2020-06-22 22:57:41 -0400

    Bound methods

--- clox/memory.c
+++ clox/memory.c
@@ -70,9 +70,17 @@ static void blackenObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_BOUND_METHOD: {
+      ObjBoundMethod* bound = (ObjBoundMethod*)object;
+      markValue(bound->receiver);
+      markObject((Obj*)bound->method);
+      break;
+    }
+
     case OBJ_CLASS: {
       ObjClass* klass = (ObjClass*)object;
       markObject((Obj*)klass->name);
+      markTable(&klass->methods);
       break;
     }
 
@@ -115,6 +123,10 @@ static void freeObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_BOUND_METHOD:
+      FREE(ObjBoundMethod, object);
+      break;
+
     case OBJ_CLASS: {
       FREE(ObjClass, object);
       break;
